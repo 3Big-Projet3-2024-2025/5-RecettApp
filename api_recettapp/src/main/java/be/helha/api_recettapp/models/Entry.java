22fff89b9763afecc8f8e12d0b4bd62eebb02b4c
Tabi Zaccaria <la226943@student.helha.be>
2024-11-18 22:36:37 +0100

    created Entry class

--- api_recettapp/src/main/java/be/helha/api_recettapp/models/Entry.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/models/Entry.java
@@ -1,4 +1,21 @@
 package be.helha.api_recettapp.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Entry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "contest_id")
+    private Contest contest;
+
+    private String status;
 }
