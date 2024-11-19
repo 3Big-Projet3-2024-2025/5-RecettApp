292f1caee66f6f12a7e416286b164b15315c7f4b
Alahayne Abdel <la216549@student.helha.be>
2024-11-19 12:30:34 +0100

    creating attributes in the user class

--- api_recettapp/src/main/java/be/helha/api_recettapp/models/User.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/models/User.java
@@ -1,4 +1,46 @@
 package be.helha.api_recettapp.models;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_user;
+
+    @Column(nullable = false)
+    private String prenom;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String motDePasse;
+
+    private String statutUtilisateur;
+
+
+    @Column(nullable = false)
+    private LocalDate dateInscription;
+    private String numeroTelephone;
+
+
+    @OneToMany(mappedBy = "user")
+    private List<Entry> inscriptions;
+
+    @OneToMany(mappedBy = "user")
+    private List<Role> roles;
+
+
 }
