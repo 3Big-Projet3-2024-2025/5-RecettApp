5ca8e08c1a6c6df54c702771756340ab1b0f7e20
Tabi Zaccaria <la226943@student.helha.be>
2024-11-20 11:28:36 +0100

    Doc Entry Class

--- api_recettapp/src/main/java/be/helha/api_recettapp/models/Entry.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/models/Entry.java
@@ -1,18 +1,32 @@
 package be.helha.api_recettapp.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * This class reprents each entry for an user to a contest
+ */
 @Data
 @Entity
 public class Entry {
+    /**
+     * Id of entry
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    /**
+     * User link to the entry
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    /**
+     * Contest link to the entry
+     */
     @ManyToOne
     @JoinColumn(name = "contest_id")
     private Contest contest;
