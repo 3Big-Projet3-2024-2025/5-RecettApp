5ebcea916bedf3742e92e9674d0b4885e00d4148
Alahayne Abdel <la216549@student.helha.be>
2024-11-24 18:43:42 +0100

    adding the method getUserById

--- api_recettapp/src/main/java/be/helha/api_recettapp/controllers/UsersController.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/controllers/UsersController.java
@@ -4,6 +4,7 @@ import be.helha.api_recettapp.models.Users;
 import be.helha.api_recettapp.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,14 @@ public class UsersController {
     @GetMapping
     public ResponseEntity<List<Users>> getAllUsers() {
         return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable Long id) {
+        Users user = userService.findById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
