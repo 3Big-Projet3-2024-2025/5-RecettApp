55957ec8ffff9de008565277cb2b61f269950a46
Alahayne Abdel <la216549@student.helha.be>
2024-11-24 18:42:19 +0100

    adding the method getAllUsers

--- api_recettapp/src/main/java/be/helha/api_recettapp/controllers/UsersController.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/controllers/UsersController.java
@@ -1,4 +1,26 @@
 package be.helha.api_recettapp.controllers;
 
+import be.helha.api_recettapp.models.Users;
+import be.helha.api_recettapp.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UsersController {
+
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Users>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
 }
