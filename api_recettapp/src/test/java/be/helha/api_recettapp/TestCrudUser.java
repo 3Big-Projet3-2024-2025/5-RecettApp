e52dfe87e2f9e2a2f788411935c3964baab5ed6a
Alahayne Abdel <la216549@student.helha.be>
2024-11-24 19:18:32 +0100

    creation of setup and testCreateUser

--- api_recettapp/src/test/java/be/helha/api_recettapp/TestCrudUser.java
+++ api_recettapp/src/test/java/be/helha/api_recettapp/TestCrudUser.java
@@ -1,2 +1,47 @@
-package be.helha.api_recettapp;public class TestCrud {
+package be.helha.api_recettapp;
+
+import be.helha.api_recettapp.models.Users;
+import be.helha.api_recettapp.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import be.helha.api_recettapp.controllers.UsersController;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UsersControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UsersController usersController;
+
+    private Users user;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        user = new Users(1L, "Abdel", "Alahyane", "abdel@gmail.com", "password", "active", LocalDate.now(), "0471772755");
+    }
+
+
+    @Test
+    void testCreateUser() {
+        when(userService.save(user)).thenReturn(user);
+
+        ResponseEntity<Users> response = usersController.createUser(user);
+
+        assertNotNull(response);
+        assertEquals("Abdel", response.getBody().getFirstName());
+
+        verify(userService, times(1)).save(user);
+    }
+
+
 }
