f5c19cae8a1b470c1399ed075ed81ffc4802a468
Alahayne Abdel <la216549@student.helha.be>
2024-11-24 18:31:57 +0100

    ajout des methodes findById , Save , delete dans userService

--- api_recettapp/src/main/java/be/helha/api_recettapp/services/UserService.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/services/UserService.java
@@ -17,4 +17,17 @@ public class UserService {
     public List<Users> findAll() {
         return userRepository.findAll();
     }
+
+    public Users findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Users save(Users user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
 }
