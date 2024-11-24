ddc6272e2091fd7098e2b4c40427d34ec0f73918
DiesbecqAaron <la229719@student.helha.be>
2024-11-24 17:54:43 +0100

    Addition of the authenticate method in the AuthentificationControler class

--- api_recettapp/src/main/java/be/helha/api_recettapp/controllers/AuthentificationControler.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/controllers/AuthentificationControler.java
@@ -1,9 +1,19 @@
 package be.helha.api_recettapp.controllers;
 
+import be.helha.api_recettapp.models.JWT;
 import be.helha.api_recettapp.utils.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,4 +40,23 @@ public class AuthentificationControler {
      */
     @Autowired
     AuthenticationManager authenticationManager;
+
+    /**
+     * Controller method for handling user authentication and returning JWT tokens.
+     *
+     * The annotation {@code @PostMapping("login")} is used to map HTTP POST requests to the "/login" URL path to this method.
+     * This endpoint is used to authenticate users.
+     */
+    @PostMapping("login")
+    public ResponseEntity<?> authenticate(@RequestParam String username, @RequestParam String password) {
+        try{
+            Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            User user = (User)auth.getPrincipal();
+            JWT jwt = new JWT(jwtUtils.generateAccessToken(user),jwtUtils.generateRefreshToken(user));
+            return ResponseEntity.ok(jwt);
+        }catch (AuthenticationException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid username or password");
+        }
+    }
 }
