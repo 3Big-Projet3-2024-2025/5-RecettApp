b3962c4a20b25d3e54b1fd97ce67734081670fee
DiesbecqAaron <la229719@student.helha.be>
2024-11-25 21:31:58 +0100

    Rename AuthenticationController to respect orthography

--- api_recettapp/src/main/java/be/helha/api_recettapp/controllers/AuthenticationController.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/controllers/AuthenticationController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-public class AuthentificationControler {
+public class AuthenticationController {
 
     /**
      * Utility class for handling JWT operations.
