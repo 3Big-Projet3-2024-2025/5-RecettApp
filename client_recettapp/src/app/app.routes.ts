fea569905b883105f4d54b4bc1d72f0c75783f0d
Alahayne Abdel <la216549@student.helha.be>
2024-12-03 22:32:56 +0100

    Creation of the front (service + model + component)

--- client_recettapp/src/app/app.routes.ts
+++ client_recettapp/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContestCategoryComponent } from './contest-category/contest-category.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
-    
+
         { path: '', redirectTo: 'Contest', pathMatch: 'full' },
         { path: 'Contest', component: ContestCategoryComponent },
-    
+        { path: 'users', component : UsersComponent},
+
 ];
