8ee704029c5e58534a9d1810c09da29b5e39b0fa
Alahayne Abdel <la216549@student.helha.be>
2024-12-08 15:22:53 +0100

    Creation addEvaluation method for service

--- api_recettapp/src/main/java/be/helha/api_recettapp/services/EvaluationService.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/services/EvaluationService.java
@@ -1,2 +1,36 @@
-package be.helha.api_recettapp.services;public class EvaluationService {
+package be.helha.api_recettapp.services;
+
+import be.helha.api_recettapp.models.Evaluation;
+import be.helha.api_recettapp.repositories.jpa.EvaluationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EvaluationService implements IEvaluationService{
+
+    @Autowired
+    private EvaluationRepository evaluationRepository;
+
+
+    @Override
+    public Evaluation addEvaluation(Evaluation evaluation) {
+        return evaluationRepository.save(evaluation);
+    }
+
+    @Override
+    public void deleteEvaluation(Long id, boolean isAdmin) {
+
+    }
+
+    @Override
+    public List<Evaluation> getAllEvaluations() {
+        return null;
+    }
+
+    @Override
+    public Evaluation updateEvaluation(Long id, Evaluation newEvaluation, boolean isAdmin) {
+        return null;
+    }
 }
