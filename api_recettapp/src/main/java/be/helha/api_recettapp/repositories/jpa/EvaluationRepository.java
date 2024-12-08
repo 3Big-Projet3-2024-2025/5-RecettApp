e10453b921d6da5deeee608886f3c98d57fcc1bc
Alahayne Abdel <la216549@student.helha.be>
2024-12-08 15:04:58 +0100

    adding EvaluationRepository

--- api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/EvaluationRepository.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/EvaluationRepository.java
@@ -1,2 +1,7 @@
-package be.helha.api_recettapp.repositories.jpa;public interface EvaluationRepository {
+package be.helha.api_recettapp.repositories.jpa;
+
+import be.helha.api_recettapp.models.Evaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 }
