629fbf0468c48548c56295a1997ee2e800e986f2
La227444 <La227444@student.helha.be>
2024-12-08 13:58:50 +0100

    recovery of the 'contestId' from the route

--- client_recettapp/src/app/add-recipe/add-recipe.component.ts
+++ client_recettapp/src/app/add-recipe/add-recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../models/recipe';
 
 @Component({
   selector: 'app-add-recipe',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './add-recipe.component.css'
 })
 export class AddRecipeComponent {
+
+  constructor(private route: ActivatedRoute){}
+  ngOnInit(): void {
+    const contestId = this.route.snapshot.paramMap.get('idConstest');
+    if (contestId != null) {
+      this.recipeToAdd.contest_id = +contestId
+    }
+  }
+  recipeToAdd: Recipe = {
+    id: 0, 
+    title: '',
+    description: '',
+    category: '',
+    preparation_time: 0,
+    cooking_time: 0,
+    servings: 0,
+    difficulty_level: '',
+    instructions: '',
+    approved: false,
+    photo_url: '',
+    contest_id: 0,
+    components: [],
+    recipe_type_id: null
+  };
 
 }
