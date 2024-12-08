a3aad7333e1db192146b4d211e7f6f69ebc2424f
La227444 <La227444@student.helha.be>
2024-12-08 20:09:21 +0100

    add the onSearch method which will search for a term in the back

--- client_recettapp/src/app/ingredient/ingredient.component.ts
+++ client_recettapp/src/app/ingredient/ingredient.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { IngredientService } from '../services/ingredient.service';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ingredient',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './ingredient.component.html',
   styleUrl: './ingredient.component.css'
 })
 export class IngredientComponent {
+  
+  searchTerm: string = ''; // sear Term
+  suggestions: any[] = []; // List of ingredient suggestions
 
+  currentIngredient: any = {
+    ingredient: null,
+    quantity: null,
+    unit: ''
+  };
+
+
+  constructor(private ingredientService: IngredientService) {}
+
+  onSearch() {
+    if (this.searchTerm.length > 2) {
+      this.ingredientService.searchIngredients(this.searchTerm).subscribe({
+        next: (data) => {
+          this.suggestions = data;
+        },
+        error: (err) => {
+          console.error('Error fetching ingredients:', err);
+        }
+      });
+    } else {
+      this.suggestions = [];
+    }
+  }
+
+  fillSearchInput(suggestion: any) {
+    this.searchTerm = suggestion.alimentName; 
+    this.currentIngredient.ingredient = suggestion; 
+    this.suggestions = []; 
+  }
+
+  
 }
