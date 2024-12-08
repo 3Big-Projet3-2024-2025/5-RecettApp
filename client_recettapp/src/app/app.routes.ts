992d478436e4a81e3563e55ca1038f63b5151903
La227444 <La227444@student.helha.be>
2024-12-08 13:32:09 +0100

    creation of the add-recipe route

--- client_recettapp/src/app/app.routes.ts
+++ client_recettapp/src/app/app.routes.ts
@@ -8,7 +8,8 @@ export const routes: Routes = [
         { path: '', redirectTo: 'Contest', pathMatch: 'full' },
         { path: 'Contest', component: ContestCategoryComponent },
         { path: 'recipe', component: RecipeComponent },
-        { path: 'recipe/:id', component: RecipeDetailComponent }
+        { path: 'recipe/:id', component: RecipeDetailComponent },
+        { path: 'recipe/add/:idConstest', component: RecipeDetailComponent }
         
     
 ];
