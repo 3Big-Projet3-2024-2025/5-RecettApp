bb3b5763a3b3eff78087bd628f16a6cd47bd02a5
Alahayne Abdel <la216549@student.helha.be>
2024-12-10 14:13:54 +0100

    adding the road for evaluation

--- client_recettapp/src/app/app.routes.ts
+++ client_recettapp/src/app/app.routes.ts
@@ -8,14 +8,16 @@ import { ContestTableComponent } from './contest-table/contest-table.component';
 import { RecipeTypeComponent } from './recipe-type/recipe-type.component';
 
 import { UsersComponent } from './users/users.component';
+import { EvaluationComponent } from './evaluation/evaluation.component';
 
 
 export const routes: Routes = [
 
         { path: '', redirectTo: 'Contest', pathMatch: 'full' },
-        { path: 'ContestCategory', component: ContestCategoryComponent , canActivate: [KeycloakGuard]},  
+        { path: 'ContestCategory', component: ContestCategoryComponent , canActivate: [KeycloakGuard]},
         { path:'RecipeType',component:RecipeTypeComponent},
         { path: 'users', component : UsersComponent},
         { path: 'contests', component: ContestTableComponent},
+        {path : 'evaluation' , component: EvaluationComponent}
 
 ];
