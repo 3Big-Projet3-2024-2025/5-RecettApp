2819357e7191fe0fefe1938ee0ad9ceaca2a3db9
La227444 <La227444@student.helha.be>
2024-12-15 20:23:09 +0100

    added 'getImage' to 'recipe-detail.component'

--- client_recettapp/src/app/recipe-detail/recipe-detail.component.ts
+++ client_recettapp/src/app/recipe-detail/recipe-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../services/recipe_Service/recipe.service';
 import { CommonModule } from '@angular/common';
 import { RecipeComponentService } from '../services/recipe_Service/recipe-component.service';
+import { ImageServiceService } from '../services/image-service.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,23 +15,50 @@ import { RecipeComponentService } from '../services/recipe_Service/recipe-compon
 })
 export class RecipeDetailComponent implements OnInit {
   recipe?: Recipe;
-
-  constructor(private route: ActivatedRoute,private router: Router, private service: RecipeService, private serviceRecipeComponent: RecipeComponentService) {}
+  imageRecipe: string | null = null;
+  constructor(private route: ActivatedRoute,private router: Router, private service: RecipeService, private serviceRecipeComponent: RecipeComponentService
+              ,private imaService: ImageServiceService
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      this.service.getRecipeById(+id).subscribe((data) => {
+      this.service.getRecipeById(+id).subscribe(
+        (data) => {
         this.recipe = data;
-      });  
-      this.serviceRecipeComponent.getRecipeComponentsByIdRecipe(+id).subscribe((data) => {
-        if (this.recipe) {
-          this.recipe.components = data;
+        if (data.photo_url) {
+          this.getImage(data.photo_url);
         }
-      });  
-
-    }
+        this.getRecipeComponent(+id)
+        },(err) => {
+          console.log(err.error.message)
+        }
+      );}
   }
+  
+  getImage(imageName: string){
+    this.imaService.getImage(imageName).subscribe(
+      (next: Blob) => {
+       
+        this.imageRecipe = URL.createObjectURL(next);
+      },
+      (err) => {
+       console.log(err.error.message)
+      }
+    );
+  }
+  getRecipeComponent(id : number){
+    this.serviceRecipeComponent.getRecipeComponentsByIdRecipe(+id).subscribe(
+      (data) => {
+      if (this.recipe) {
+        this.recipe.components = data;
+      }
+    }, (err) => {
+      console.log(err.error.message)
+     }
+    ); 
+  }
+
   backRecipeList(): void {
     this.router.navigate(['/recipe']);
   }
