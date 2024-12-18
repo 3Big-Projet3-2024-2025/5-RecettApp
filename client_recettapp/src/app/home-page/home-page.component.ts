add807f7c5b77ba6e0b661e4c502f000fab77ee5
La227444 <La227444@student.helha.be>
2024-12-18 11:25:27 +0100

    creating the navbar component

--- client_recettapp/src/app/home-page/home-page.component.ts
+++ client_recettapp/src/app/home-page/home-page.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RecipeService } from '../services/recipe_Service/recipe.service';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Recipe } from '../models/recipe';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
