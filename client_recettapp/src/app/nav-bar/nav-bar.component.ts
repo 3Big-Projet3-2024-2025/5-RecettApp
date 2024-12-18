cc3988c0af41efa9e40c67ef0c389eb62ee9d16e
La227444 <La227444@student.helha.be>
2024-12-18 14:48:50 +0100

    add disconnect button to navbar

--- client_recettapp/src/app/nav-bar/nav-bar.component.ts
+++ client_recettapp/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthKeycloakService } from '../services/auth-keycloak.service';
+import { KeycloakService } from 'keycloak-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  constructor(private authService : AuthKeycloakService, private keycloakService: KeycloakService){}
+  isAuhtentificated= false;
 
+  ngOnInit(){
+       this.isAuhtentificated = this.authService.isAuthenticated()
+  }
+
+  logOut(int : number){
+    this.authService.logout();
+    console.log("bien loguer")
+  }
 }
