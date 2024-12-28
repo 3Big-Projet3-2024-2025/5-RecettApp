0866c07553e80dc624cd7642e67eecce4c67c03b
La227444 <La227444@student.helha.be>
2024-12-28 20:40:43 +0100

    Add the ngOnInit methode to list all recipes belonging to a specific contest

--- client_recettapp/src/app/recipe-contest-list/recipe-contest-list.component.ts
+++ client_recettapp/src/app/recipe-contest-list/recipe-contest-list.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Recipe } from '../models/recipe';
+import { Router } from '@angular/router';
+import { map, of, forkJoin } from 'rxjs';
+import { ImageServiceService } from '../services/image-service.service';
+import { RecipeService } from '../services/recipe_Service/recipe.service';
 
 @Component({
   selector: 'app-recipe-contest-list',
@@ -10,5 +15,40 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './recipe-contest-list.component.css'
 })
 export class RecipeContestListComponent {
+
+recipes: Recipe[] = [];
+constructor(private service: RecipeService, private imaService: ImageServiceService) {}
+
+
+   ngOnInit(): void {
+    
+    this.service.getRecipeByIdContest(32).subscribe(data => {
+      const imageRequests = data.map(recipe => {
+        if (recipe.photo_url) {
+          return this.imaService.getImage(recipe.photo_url).pipe(
+            map((blob: Blob) => {
+              recipe.photo_url = URL.createObjectURL(blob);
+              return recipe;  
+            })
+          );
+        }
+        return of(recipe);  
+      });
+      
+      forkJoin(imageRequests).subscribe((updatedRecipes) => {
+        this.recipes = updatedRecipes;
+      });
+    });
+  }
+
+confirmDelete(arg0: any,arg1: any) {
+throw new Error('Method not implemented.');
+}
+detailRecipe(arg0: any) {
+throw new Error('Method not implemented.');
+}
+addRecipe() {
+throw new Error('Method not implemented.');
+}
 
 }
