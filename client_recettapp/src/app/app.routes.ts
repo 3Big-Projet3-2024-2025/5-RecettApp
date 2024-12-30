522ea7a7bf8521b400a55e5623fc54a6f05cb2c1
Alahayne Abdel <la216549@student.helha.be>
2024-12-30 16:05:56 +0100

    modification of roads

--- client_recettapp/src/app/app.routes.ts
+++ client_recettapp/src/app/app.routes.ts
@@ -9,31 +9,16 @@ import { RecipeTypeComponent } from './recipe-type/recipe-type.component';
 import { UsersComponent } from './users/users.component';
 
 import { EntriesTableComponent } from './entries-table/entries-table.component';
-<<<<<<< HEAD
-import { HomePageComponent } from './home-page/home-page.component';
 import { EvaluationComponent } from './evaluation/evaluation.component';
-=======
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AvailableContestComponent } from './available-contest/available-contest.component';
->>>>>>> e3c98322e8f20019692ebbd20d7fea722835754b
 
 
 
 export const routes: Routes = [
 
         { path: '', redirectTo: 'home', pathMatch: 'full' },
-<<<<<<< HEAD
-        { path: 'recipe', component: RecipeComponent },
-        { path: 'recipe/:id', component: RecipeDetailComponent },
-        { path: 'recipe/add/:idConstest', component: AddRecipeComponent },
-        { path: 'ContestCategory', component: ContestCategoryComponent , canActivate: [KeycloakGuard]},
-        { path:'RecipeType',component:RecipeTypeComponent},
-        { path: 'users', component : UsersComponent},
-        { path: 'contests', component: ContestTableComponent},
-        { path: 'entries', component: EntriesTableComponent},
-        { path: 'home', component: HomePageComponent},
-        {path : 'evaluation' , component: EvaluationComponent}
-=======
+
         { path: 'recipe', component: RecipeComponent , canActivate: [KeycloakGuard]},
         { path: 'recipe/:id', component: RecipeDetailComponent , canActivate: [KeycloakGuard]},
         { path: 'recipe/add/:idEntry', component: AddRecipeComponent , canActivate: [KeycloakGuard]},
@@ -43,8 +28,7 @@ export const routes: Routes = [
         { path: 'contests', component: ContestTableComponent , canActivate: [KeycloakGuard]},
         { path: 'entries', component: EntriesTableComponent , canActivate: [KeycloakGuard]},
         { path: 'available-contests', component: AvailableContestComponent, canActivate: [KeycloakGuard]},
-        { path: 'home', component: HomePageComponent}
-
->>>>>>> e3c98322e8f20019692ebbd20d7fea722835754b
+        { path: 'home', component: HomePageComponent},
+        {path : 'evaluation' , component: EvaluationComponent}
 
 ];
