556db575e766bb5e018bbfdc450fcc9273d9e843
Alahayne Abdel <la216549@student.helha.be>
2024-12-30 16:08:24 +0100

    modification of the evaluation model in the front end

--- client_recettapp/src/app/models/evaluation.ts
+++ client_recettapp/src/app/models/evaluation.ts
@@ -4,8 +4,8 @@ import { RecipeType } from "./recipe-type";
 import { User } from "./users";
 
 export interface Evaluation {
-  id?: number;
-  rate?: number;
+  id: number;
+  rate: number;
   entry?: Entry;
   recipe?: Recipe;
 }
