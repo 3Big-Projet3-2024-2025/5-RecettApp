b093cce8701e6876387b9a13767f6301cd9902ec
Alahayne Abdel <la216549@student.helha.be>
2024-12-30 16:09:51 +0100

    User model

--- api_recettapp/src/main/java/be/helha/api_recettapp/models/Users.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/models/Users.java
@@ -52,10 +52,7 @@ public class Users {
      * This field cannot be null.
      * For security, passwords should be encrypted.
      */
-<<<<<<< HEAD
 
-=======
->>>>>>> e3c98322e8f20019692ebbd20d7fea722835754b
     @Column(nullable = false)
     private String password;
 
