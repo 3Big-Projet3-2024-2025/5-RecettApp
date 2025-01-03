26a688388a7cb13049c7d55ea04492f5585cce82
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-03 20:01:20 +0100

    Add route for user-info

--- client_recettapp/src/app/app.routes.ts
+++ client_recettapp/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { AvailableContestComponent } from './available-contest/available-contest
 import { PaypalSuccessComponent } from './paypal-success/paypal-success.component';
 import { PaypalCancelComponent } from './paypal-cancel/paypal-cancel.component';
 import { RecipeContestListComponent } from './recipe-contest-list/recipe-contest-list.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 
 
@@ -31,7 +32,8 @@ export const routes: Routes = [
         { path: 'available-contests', component: AvailableContestComponent, canActivate: [KeycloakGuard]},
         { path: 'success', component:PaypalSuccessComponent},
         { path: 'cancel', component:PaypalCancelComponent},
-        { path: 'home', component: HomePageComponent}
+        { path: 'home', component: HomePageComponent},
+        {path:'User-info',component:UserInfoComponent}
 
 
 ];
