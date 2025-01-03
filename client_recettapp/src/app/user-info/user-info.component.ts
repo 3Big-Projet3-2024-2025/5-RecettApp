dd5564b323485158a228f9060b7f47cd14ae06ec
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-03 19:56:40 +0100

    add a method to get userDetails

--- client_recettapp/src/app/user-info/user-info.component.ts
+++ client_recettapp/src/app/user-info/user-info.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import {jwtDecode} from 'jwt-decode';
+import { UsersService } from '../services/users.service';
+import { User } from '../models/users';
 
 @Component({
   selector: 'app-user-info',
-  standalone: true,
-  imports: [],
   templateUrl: './user-info.component.html',
-  styleUrl: './user-info.component.css'
+  styleUrls: ['./user-info.component.css']
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
+  user!: User; 
+  userName!: string; 
 
+  constructor(
+    private keycloakService: KeycloakService,
+    private userService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUserDetail();
+  }
+
+  
+  getUserDetail(): void {
+    this.keycloakService.getToken()
+      .then(token => {
+        const decodedToken: any = jwtDecode(token);
+        const email = decodedToken.email;
+        this.userName = decodedToken.preferred_username; 
+        this.getUserByEmail(email); 
+      })
+      .catch(err => {
+        console.error("Erreur lors de la récupération des informations utilisateur :", err);
+      });
+  }
 }
