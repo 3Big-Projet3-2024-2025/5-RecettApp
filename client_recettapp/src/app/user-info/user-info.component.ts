759df7a1b3bc25f45cf203ca46ab053ef70a4ab1
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-03 20:15:23 +0100

    Added some logs for the console

--- client_recettapp/src/app/user-info/user-info.component.ts
+++ client_recettapp/src/app/user-info/user-info.component.ts
@@ -3,9 +3,13 @@ import { KeycloakService } from 'keycloak-angular';
 import {jwtDecode} from 'jwt-decode';
 import { UsersService } from '../services/users.service';
 import { User } from '../models/users';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-info',
+  standalone:true,
+  imports:[FormsModule,CommonModule],
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.css']
 })
@@ -26,24 +30,29 @@ export class UserInfoComponent implements OnInit {
   getUserDetail(): void {
     this.keycloakService.getToken()
       .then(token => {
+        console.log("Token reçu :", token); 
         const decodedToken: any = jwtDecode(token);
         const email = decodedToken.email; 
+        console.log("Email décodé :", email);
         this.userName = decodedToken.preferred_username; 
+        this.getUserByEmail(email); 
       })
       .catch(err => {
-        console.error("Erreur lors de la récupération des informations utilisateur :", err);
+        console.error("Erreur lors de la récupération du token :", err);
       });
   }
+  
 
   getUserByEmail(email: string): void {
     this.userService.findByEmail(email).subscribe({
       next: (user) => {
-        this.user = user; // Affecter les données utilisateur
-        console.log("Données utilisateur :", this.user);
+        console.log("Utilisateur récupéré :", user); 
+        this.user = user;
       },
       error: (err) => {
         console.error("Erreur lors de la récupération des informations utilisateur :", err);
       }
     });
   }
+  
 }
