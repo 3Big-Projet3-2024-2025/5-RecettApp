fa5e85151530fff931b06cc401d15995cd5c6573
Tabi Zaccaria <la226943@student.helha.be>
2025-01-04 02:28:27 +0100

    add method jpa findEntriesByContestId and findEntriesByUsersId

--- api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/EntryRepository.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/EntryRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.List;
 import java.util.UUID;
 
 import java.util.Optional;
@@ -21,7 +22,8 @@ public interface EntryRepository extends JpaRepository<Entry, Integer> {
     @param uiid the universal unique identifier
      */
     Entry findByUuid(UUID uuid);
-
+    List<Entry> findEntriesByContestId(Integer idContest);
+    List<Entry> findEntriesByUsersId(long idUsers);
 
     @Query("SELECT e FROM Entry e WHERE e.contest.id = :idContest AND e.users.email = :userMail")
     Optional<Entry> findByContestIdAndUserEmail(@Param("idContest") int idContest, @Param("userMail") String userMail);
