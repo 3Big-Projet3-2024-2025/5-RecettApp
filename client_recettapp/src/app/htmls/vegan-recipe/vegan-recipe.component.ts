50735704c944d2c09e5f10a17981045339d7e847
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-04 15:20:45 +0100

    add component vegan recipe

---
+++ client_recettapp/src/app/htmls/vegan-recipe/vegan-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vegan-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './vegan-recipe.component.html',
+  styleUrl: './vegan-recipe.component.css'
+})
+export class VeganRecipeComponent {
+
+}
