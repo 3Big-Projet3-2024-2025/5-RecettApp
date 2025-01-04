9e19498c5f32b7e7fdbda6d3579d85415b4ededd
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-04 15:14:39 +0100

    add component apple pie

---
+++ client_recettapp/src/app/htmls/apple-pie/apple-pie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apple-pie',
+  standalone: true,
+  imports: [],
+  templateUrl: './apple-pie.component.html',
+  styleUrl: './apple-pie.component.css'
+})
+export class ApplePieComponent {
+
+}
