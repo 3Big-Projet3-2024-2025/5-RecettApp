4fe366c1ca9e36f7b999c708ed825f8e7f7bcee1
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-04 15:18:02 +0100

    add component apple pie

---
+++ client_recettapp/src/app/htmls/chicken-breast/chicken-breast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chicken-breast',
+  standalone: true,
+  imports: [],
+  templateUrl: './chicken-breast.component.html',
+  styleUrl: './chicken-breast.component.css'
+})
+export class ChickenBreastComponent {
+
+}
