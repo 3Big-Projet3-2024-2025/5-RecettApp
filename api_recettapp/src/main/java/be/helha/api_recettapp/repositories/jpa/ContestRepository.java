ca097b11102952e1b5e67e2adce666ffc2b63c1d
Tabi Zaccaria <la226943@student.helha.be>
2025-01-04 10:53:44 +0100

    Add service and controler of Paginated Contests

--- api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/ContestRepository.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/repositories/jpa/ContestRepository.java
@@ -2,11 +2,12 @@ package be.helha.api_recettapp.repositories.jpa;
 
 import be.helha.api_recettapp.models.Contest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ContestRepository extends JpaRepository<Contest, Integer> {
+public interface ContestRepository extends JpaRepository<Contest, Integer>, PagingAndSortingRepository<Contest, Integer> {
     public List<Contest> findByTitle(String title);
 }
