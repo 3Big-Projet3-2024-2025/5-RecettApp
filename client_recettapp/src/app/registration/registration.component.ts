80759160f29730a4ff8aaf89bfef971443f61c20
DiesbecqAaron <la229719@student.helha.be>
2025-01-04 21:08:57 +0100

    Addition of the token in API call methods of the registration component

--- client_recettapp/src/app/registration/registration.component.ts
+++ client_recettapp/src/app/registration/registration.component.ts
@@ -74,12 +74,13 @@ export class RegistrationComponent {
 
 
 
-  pay():void{
+  async pay(): Promise<void> {
     this.entry.contest = this.contest;
     this.entry.users = this.user;
     this.entry.status = "process";
     const price = 20
-    this.paypalService.payToRegister(price, this.entry).subscribe({
+    const token = await this.keycloakService.getToken();
+    this.paypalService.payToRegister(price, this.entry, token).subscribe({
       next: (approvalUrl) => {
         console.log('Redirecting to PayPal URL:', approvalUrl);
         this.errMsg = "";
