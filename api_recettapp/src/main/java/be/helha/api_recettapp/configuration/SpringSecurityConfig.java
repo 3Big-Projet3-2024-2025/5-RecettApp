e9032c127af289b685c3b16bea9b87f015cca9b8
DiesbecqAaron <la229719@student.helha.be>
2025-01-04 22:46:46 +0100

    Correction of security filters SpringBoot

--- api_recettapp/src/main/java/be/helha/api_recettapp/configuration/SpringSecurityConfig.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/configuration/SpringSecurityConfig.java
@@ -34,30 +34,51 @@ public class SpringSecurityConfig {
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/contest-categories").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/contest-categories").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/contest-categories").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/contests").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.POST,"/contests").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/contests").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/entries").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/entries").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/evaluations").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/evaluations").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/evaluations").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/ingredients").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/ingredients").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.POST,"/ingredients").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/recipe").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/recipe-types").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/recipe-types").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/recipe-types").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/users").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/contest-categories/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/contest-categories/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/contest-categories/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/contest-categories/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/contest-categories/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/contests/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/contests/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/contests/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/contests/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/entries/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/entries/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/entries/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/entries/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/evaluations/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/evaluations/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/evaluations/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/evaluations/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/ingredients/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/ingredients/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/ingredients/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/ingredients/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/recipe/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/recipe/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/recipe/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/recipe/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/recipe-types/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/recipe-types/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/recipe-types/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/recipe-types/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/users/*").hasRole("ADMIN");
                     authorizeRequests.requestMatchers(HttpMethod.POST,"/api/users/{email}/unblock").hasRole("ADMIN");
                     authorizeRequests.requestMatchers(HttpMethod.POST,"/api/users/{email}/block").hasRole("ADMIN");
-                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/users").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/api/users/*").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/api/users/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/api/users/{id}").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/paypal/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/paypal/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/image/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/image/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET,"/recipe-components/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/recipe-components/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT,"/recipe-components/*").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/recipe-components/*").hasAnyRole("ADMIN", "USER");
 
-                    authorizeRequests.anyRequest().authenticated();
+                    authorizeRequests.anyRequest().permitAll();
                 }).oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 ).build();
