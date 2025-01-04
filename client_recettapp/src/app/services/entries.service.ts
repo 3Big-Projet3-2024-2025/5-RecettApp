1b6752a45bbd01e5270bba681413a3c7e4a00d12
Alahayne Abdel <la216549@student.helha.be>
2025-01-04 16:02:13 +0100

    changes to the entries service

--- client_recettapp/src/app/services/entries.service.ts
+++ client_recettapp/src/app/services/entries.service.ts
@@ -40,7 +40,7 @@ export class EntriesService {
         return this.http.get<Entry>(url);
       })
     );
-        
+
   }
   async getUserMail(){
      const token = await this.authService.getToken();
