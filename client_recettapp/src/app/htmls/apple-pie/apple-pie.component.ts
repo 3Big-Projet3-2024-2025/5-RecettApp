9fab94e269f239b93d66f0373a749fc97ab51c6d
la210006 <127667940+la210006@users.noreply.github.com>
2025-01-04 17:15:00 +0100

    Optimize Apple Pie component logic for better performance

--- client_recettapp/src/app/htmls/apple-pie/apple-pie.component.ts
+++ client_recettapp/src/app/htmls/apple-pie/apple-pie.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-apple-pie',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,RouterLink],
   templateUrl: './apple-pie.component.html',
   styleUrl: './apple-pie.component.css'
 })
