4f9f7b0f19feba348c1162b906ce284621ada82c
DiesbecqAaron <la229719@student.helha.be>
2025-01-04 11:16:22 +0100

    Remove all that refer to authentication without Keycloak

--- api_recettapp/src/main/java/be/helha/api_recettapp/configuration/SpringSecurityConfig.java
+++ api_recettapp/src/main/java/be/helha/api_recettapp/configuration/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package be.helha.api_recettapp.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -48,36 +49,11 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests(authorizeRequests -> {
                     // Permits all requests to all endpoints for the moment, to easily test API
                     //authorizeRequests.requestMatchers("/api/users").authenticated();
+                    authorizeRequests.requestMatchers("/api/contest-categories").hasRole("USER");
                     authorizeRequests.anyRequest().permitAll();
                 }).oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 ).build();
-    }
-
-    /**
-     * Bean definition for the password encoder used to encode passwords.
-     *
-     * @return A {@link PasswordEncoder} bean (specifically {@link BCryptPasswordEncoder}).
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    /**
-     * Bean definition for the authentication manager used to authenticate users.
-     *
-     * @param http The {@link HttpSecurity} instance, used to configure HTTP security settings.
-     * @param passwordEncoder The {@link PasswordEncoder} used to validate passwords.
-     * @return An {@link AuthenticationManager} bean, used to authenticate users based on their credentials.
-     * @throws Exception If there is an error while configuring the authentication manager.
-     */
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuiler = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuiler.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-        return authenticationManagerBuiler.build();
     }
 
     /**
